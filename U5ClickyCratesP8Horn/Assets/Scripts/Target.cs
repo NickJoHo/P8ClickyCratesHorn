66a7907ba44ea6694de671a88473277a3e338c24
Rafaelakamexican <739415@student.dusd.net>
2025-05-14 15:27:28 -0700

    day one 5/14/25
    downloaded zip and did half of 5.1

---
+++ U5ClickyCratesP8Horn/Assets/Scripts/Target.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    private Rigidbody targetRb;
+    private float minSpeed = 12;
+    private float maxSpeed = 16;
+    private float maxTorque = 10;
+    private float xRange = 4;
+    private float yRange = -6;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //fix void start codes via q.4
+        targetRb = GetComponent<Rigidbody>();
+        targetRb.AddForce(Vector3.up * Random.RandomRange(12, 16), ForceMode.Impulse);
+        targetRb.AddTorque(Random.Range(-10, 10), Random.Range(-10, 10);
+        Random.Range(-10, 10), ForceMode.Impulse);
+        transform.position = new Vector3(Random.Range(-4, 4), -6);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
