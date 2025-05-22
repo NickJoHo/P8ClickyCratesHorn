6a3fe23b24df8d3c56847eecd7564747dca37a8c
Rafaelakamexican <739415@student.dusd.net>
2025-05-22 15:03:11 -0700

    5/22/2025
    having problems in 5.2 with code being wrong and objections not spawning added "brat" ui

--- U5ClickyCratesP8Horn/Assets/Scripts/GameManager.cs
+++ U5ClickyCratesP8Horn/Assets/Scripts/GameManager.cs
@@ -1,21 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UIElements;
 
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
+    public TextMeshProUGUI scoreText;
+    private int score;
     private float spawnRate = 1.0f;
+    private int scoreToAdd;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(SpawnTarget());
+        score = 0;
+        UpdateScore(0);
+    }
+
+    private void UpdateScore(int v)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void StartCoroutine(IEnumerable enumerable)
+    {
+        throw new System.NotImplementedException();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // use 5.2 to try to fix
     }
     IEnumerable SpawnTarget()
     {
@@ -25,5 +43,11 @@ public class GameManager : MonoBehaviour
             int index = Random.Range(0, targets.Count);
             Instantiate(targets[index]);
         }
+         void UpdateScore()
+        {
+            score += scoreToAdd;
+            scoreText.text = "Score: " + score;
+        }
     }
 }
+
